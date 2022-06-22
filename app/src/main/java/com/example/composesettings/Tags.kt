b2adb6dc8dc1c06b7ba6c40b44edcef047f64e83
Amit Singh <amit.mot.08@gmail.com>
2022-06-23 07:51:05 +1000

    Theme settings test

--- app/src/main/java/com/example/composesettings/Tags.kt
+++ app/src/main/java/com/example/composesettings/Tags.kt
@@ -2,4 +2,7 @@ package com.example.composesettings
 
 object Tags {
     const val TAG_TOGGLE_ITEM = "toggle_item"
+    const val TAG_THEME = "theme"
+    const val TAG_SELECT_THEME = "select_theme"
+    const val TAG_THEME_OPTION = "theme_"
 }
